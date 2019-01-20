5e46c1476ddd67f7af86c4838dc446af290d97a2
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2019-01-20 22:49:36 +0100

    tests: Cloning cura-4.0.0-updates branch

--- tests/build-source.sh
+++ tests/build-source.sh
@@ -3,6 +3,6 @@
 
 URL="https://thopiekar.eu:5443/cura-cad-integration/CuraBlenderPlugin.git"
 SOURCE="./source"
-git clone $URL $SOURCE
+git clone $URL -b cura-4.0.0-updates $SOURCE --recurse-submodules
 
 python3 ../cpo.py --create=all --source=$SOURCE

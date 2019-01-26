10ca9b594042b6edb54459c36a96cb8d82f0ee3c
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2019-01-26 21:11:43 +0100

    Updating tests

--- tests/build-git.sh
+++ tests/build-git.sh
@@ -3,4 +3,4 @@
 
 SOURCE="https://thopiekar.eu:5443/cura-cad-integration/CuraBlenderPlugin.git"
 
-python3 ../cpo.py --create=all --source=$SOURCE --gitargs="-b cura-4.0.0-updates"
+python3 ../cpo.py --create=all --source=$SOURCE
